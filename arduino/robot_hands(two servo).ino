2e575e76119a63e031b76369ec7b727ae420d274
Jason Juhyun Kim <mtinet@hanmail.net>
2020-07-12 14:55:12 +0900

    Create robot_hands(two servo).ino

---
+++ arduino/robot_hands(two servo).ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+
+Servo finger1;  
+Servo finger2;
+
+
+void setup() {
+  finger1.attach(11);  
+  finger2.attach(10);
+  Serial.begin(9600);
+
+  //initialize
+  finger1.write(0);
+  finger2.write(0);
+  delay(100); 
+}
+
+
+
+void loop() {  
+  if (Serial.available()) {
+    char input = Serial.read();
+    
+    if (input == 'a') {
+      paper();
+    } else if (input == 'b') {
+      scissors();
+    } else if (input == 'c') {
+      rock();
+    }
+  }
+}
+
+
+void rock() {
+  finger1.write(0);
+  finger2.write(0);
+  delay(100);  
+}
+
+void paper() {
+  finger1.write(180);
+  finger2.write(180);
+  delay(100);  
+}
+
+void scissors() {
+  finger1.write(0);
+  finger2.write(180);
+  delay(100);  
+}
